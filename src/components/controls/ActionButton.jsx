1e71a1ac81417f7cba362056187f7c5880314c9f
Cash Myers <cmyers880@gmail.com>
2022-06-12 20:11:47 -0500

    feat: Added optional Tooltip attribute

--- src/components/controls/ActionButton.jsx
+++ src/components/controls/ActionButton.jsx
@@ -1,46 +1,34 @@
 import React from 'react';
-import { Button, useTheme } from '@mui/material';
-
-
+import { Button, Tooltip, useTheme } from '@mui/material';
 
 export default function ActionButton(props) {
-
-    const { color, children, onClick, ...other } = props;
-
+    const { color, children, onClick, tooltipText, ...other } = props;
     const theme = useTheme();
 
-    // const colorPalette = (color) => {
-    //     switch (color) {
-    //         case "primary":
-    //             return color = theme.palette.primary.main
-    //         case "secondary":
-    //             return color = theme.palette.secondary.main
-    //         case "error":
-    //             return color = theme.palette.error.main
-    //         case "warning":
-    //             return color = theme.palette.warning.main
-    //         case "info":
-    //             return color = theme.palette.info.main
-    //         case "success":
-    //             return color = theme.palette.success.main
-    //         default:
-    //             return ""
-    //     }
-    // }
-
-
     return (
-        <Button
-            sx={{
-                minWidth: 0,
-                margin: theme.spacing(0.5)
-            }}
-            style={{ color: color }}
-            // {...colorPalette(color)}
-            onClick={onClick}
-            {...other}
-        >
-            {children}
-        </Button >
+        <>
+            {tooltipText &&
+                <Tooltip title={tooltipText}>
+                    <Button
+                        sx={{ minWidth: 0, margin: theme.spacing(0.5) }}
+                        style={{ color: color }}
+                        onClick={onClick}
+                        {...other}
+                    >
+                        {children}
+                    </Button >
+                </Tooltip>
+            }
+            {!tooltipText &&
+                <Button
+                    sx={{ minWidth: 0, margin: theme.spacing(0.5) }}
+                    style={{ color: color }}
+                    onClick={onClick}
+                    {...other}
+                >
+                    {children}
+                </Button >
+            }
+        </>
     )
 }
